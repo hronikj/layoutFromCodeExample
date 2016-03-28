775cf64837c47783f397cf28ec438a6aeec9d467
Jiří Hroník <jiri@hronik.cz>
2016-03-28 21:43:42 +0200

    Initial Commit

---
+++ LayoutFromTheCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutFromTheCode
+//
+//  Created by Jiří Hroník on 28/03/16.
+//  Copyright © 2016 Jiří Hroník. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
